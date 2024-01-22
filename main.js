f2d498c70ad225b08bc7d4b0e80c54db390c8fce
tawas-webpage <145882526+tawas-webpage@users.noreply.github.com>
2024-01-23 11:05:21 +1300

    Update main.js

--- main.js
+++ main.js
@@ -9,3 +9,17 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+
+document.addEventListener(
+    "click",
+    function(event) {
+        var target = event.target;
+        var replyForm;
+        if (target.matches("[data-toggle='reply-form']")) {
+            replyForm = document.getElementById(target.getAttribute("data-target"));
+            replyForm.classList.toggle("d-none");
+        }
+    },
+    false
+);
