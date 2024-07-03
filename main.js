c1ae72a93db64058260392e69aa1d0a70802864b
tawas-webpage <145882526+tawas-webpage@users.noreply.github.com>
2024-07-04 10:41:49 +1200

    Update main.js

--- main.js
+++ main.js
@@ -83,3 +83,31 @@ function closeNav() {
 }
 
 
+
+
+
+
+
+
+
+
+    
+
+    document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('.top-nav a');
+
+    for (const link of links) {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            window.scrollTo({
+                top: targetElement.offsetTop - 50, // Adjust this value based on your nav bar height
+                behavior: 'smooth'
+            });
+        });
+    }
+});
+
