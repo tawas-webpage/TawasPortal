b01f6fa93a3a1f7d42be2a9e38576d595d465ba2
tawas-webpage <145882526+tawas-webpage@users.noreply.github.com>
2024-07-04 15:51:50 +1200

    Update main.js

--- main.js
+++ main.js
@@ -88,7 +88,14 @@ function closeNav() {
 
 
 
+document.addEventListener('DOMContentLoaded', function () {
+    const navToggle = document.querySelector('.nav-toggle');
+    const navMenu = document.querySelector('.nav-menu');
 
+    navToggle.addEventListener('click', function () {
+        navMenu.classList.toggle('nav-menu_visible');
+    });
+});
 
 
     
